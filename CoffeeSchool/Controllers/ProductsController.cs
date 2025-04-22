53b31d26431c22163822a4ddf8492d45fe76f1c0
KatHuyn <KatHuyn@DESKTOP-UB4NCR2>
2025-04-22 19:29:42 +0700

    Add project files.

---
+++ CoffeeSchool/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿
+using CoffeeShop.Models.interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Shop()
+        {
+            return View(productRepository.GetAllProducts());
+        }
+    }
+}
---
+++ CoffeeSchool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoffeeSchool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
